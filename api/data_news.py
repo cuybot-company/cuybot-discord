3abb5ba95f937a14f129435699934cd70da51787
unknown <juheri842@gmail.com>
2021-10-06 13:53:55 +0700

    bugfix

--- api/data_news.py
+++ api/data_news.py
@@ -7,11 +7,13 @@ def data_news():
     news = datas['data']['posts']
     
     if len(news) > 0 :
+        datas = []
         for data in news:
             title = data['title']
             link = data['link']
             date = data['pubDate']
             description = data['description']
-            return('Berita teknologi hari ini: \n' + title + '\n\n' + link + '\n' + date + '\n' + description + '\n\n')
+            datas.append('Berita teknologi hari ini: \n' + title + '\n\n' + link + '\n' + date + '\n' + description + '\n\n')
+        return(''.join(datas))
     else:
         return('saat ini tidak ada data berita')
