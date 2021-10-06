d9c2a0b87c09a29e2589c4f8b39e864b380981ee
unknown <juheri842@gmail.com>
2021-10-06 20:30:33 +0700

    bugfix news kategory

--- api/data_news.py
+++ api/data_news.py
@@ -2,16 +2,17 @@ import requests
 import json
 
 def data_news(param):
-    data_news =  requests.get('https://api-berita-indonesia.vercel.app/cnn/' + param)
+    data_param = param.split()[1]
+    data_news =  requests.get('https://api-berita-indonesia.vercel.app/cnn/' + data_param)
     datas = json.loads(data_news.text)
     news = datas['data']
     
-    if len(news) > 0 :
-        for data in news:
-            title = data['posts']['title']
-            link = data['posts']['link']
-            date = data['posts']['pubDate']
-            description = data['posts']['description']
-            return('Berita '+param+'hari ini: \n' + title + '\n\n' + link + '\n' + date + '\n' + description + '\n\n')
+    if news is None :
+        return('saat ini kategori berita ' +data_param+' tidak ada\n\nmasukkan kategori berita, contoh: \n`/berita nasional`\n`/berita internasional`\n`/berita ekonomi`\n`/berita olahraga`\n`/berita teknologi`\n`/berita hiburan`')
     else:
-        return('saat ini tidak ada data berita')
+        title = news['posts'][0]['title']
+        link = news['posts'][0]['link']
+        date = news['posts'][0]['pubDate']
+        description = news['posts'][0]['description']
+        return('Berita '+param+' hari ini: \n' + title + '\n\n' + link + '\n' + date + '\n' + description + '\n\n')
+    
--- response/news.py
+++ response/news.py
@@ -8,9 +8,9 @@ class news(object):
 
     async def find_one(self):
         if self.user_message.startswith('cuy/berita'):
-            parameter = self.user_message.split()[1]
-            if len(parameter) == 0:
-                await self.bot_send('masukkan kategori berita, contoh: \nnasional\ninternasional\nekonomi\nolahraga\nteknologi\nhiburan')
-            else :
-                daftar_news = api.data_news(parameter)
+            parameter = self.user_message.split()
+            if len(parameter) == 1:
+                await self.bot_send('masukkan kategori berita, contoh: \n`/berita nasional`\n`/berita internasional`\n`/berita ekonomi`\n`/berita olahraga`\n`/berita teknologi`\n`/berita hiburan`')
+            else:
+                daftar_news = api.data_news(self.user_message)
                 await self.bot_send(daftar_news)
