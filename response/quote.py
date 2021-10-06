0b639165625265a67e8a51bc0946b5a53cad4062
Tedi Suryadi <32906420+Teday@users.noreply.github.com>
2021-10-06 05:59:27 +0000

    add keyword quote

--- response/quote.py
+++ response/quote.py
@@ -1,11 +1,12 @@
 import api.data_quote as api
+import helper.constants as c
 
 class quote(object):
   def __init__(self, user_message, bot_send):
     self.user_message = user_message
     self.bot_send = bot_send
   async def find_one(self):
-        if self.user_message.startswith('cuy/quotes'):
+        if any(x in self.user_message for x in c.request_quote):
             await self.bot_send('Tunggu sebentar saya carikan dulu quotes menarik untuk kamu ')
             data = api.get_quotes()
             await self.bot_send(data)
