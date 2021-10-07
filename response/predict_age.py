53dcf16b90670e249d562361f088d2b821313a2b
dea <dea.aprizal@icloud.com>
2021-10-07 15:16:21 +0700

    fix reply

--- response/predict_age.py
+++ response/predict_age.py
@@ -10,7 +10,7 @@ class predict_age(object):
         data = self.user_message.split(" ", 1)
         if len(data) == 1:
             await self.bot_send(':x: Masukin dulu nama lu cuy :x:')
-        elif len(data) > 1:
+        elif len(data) > 2:
             await self.bot_send(':x: Hanya support menebak 1 kata nama (nama panggilan atau real name) :x:')
         else:
             requested_name = self.user_message.split(" ", 1)[1]
