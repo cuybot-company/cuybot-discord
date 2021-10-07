c849bb2b48a0bd31049ff5cb0cb539c6e9f7d702
dea <dea.aprizal@icloud.com>
2021-10-07 15:08:25 +0700

    fix reply

--- api/data_age_prediction.py
+++ api/data_age_prediction.py
@@ -6,4 +6,4 @@ def predict(name):
     yourname = requests.get(
         'https://api.agify.io/?name=' + name)
     datas = json.loads(yourname.text)
-    return('Hi ' + datas['name'] + '!\n' + 'cuybot nebak kalau umur lu keknya ' + '**' + datas['age'] +'**' + '\n\nhehe peace :P maaf kalau salah, thank u kalau bener :)')
+    return('Hi ' + str(datas['name']) + '!\n' + 'cuybot nebak kalau umur lu keknya ' + '**' + str(datas['age']) +'**' + '\n\nhehe peace :P maaf kalau salah, thank u kalau bener :)')
