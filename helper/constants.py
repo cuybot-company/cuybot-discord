795ccd7cce8f99bfbd21d3d054f5d18ee8e48b30
dea <dea.aprizal@icloud.com>
2021-10-09 13:35:25 +0700

    securing token

--- helper/constants.py
+++ helper/constants.py
@@ -10,9 +10,10 @@ request_mobile = ["cuy/mobile", "cuy/hp", "cuy/handphone", "cuy/phone"]
 request_new_mobile = ["baru", "terbaru", "new"]
 request_g_credentials = {
     "type": os.getenv("G_TYPE"),
-    "project_id": os.getenv("PROJECT_ID"),
+    "project_id": os.getenv("G_PROJECT_ID"),
     "private_key_id": os.getenv("G_PRIVATE_KEY_ID"),
     "private_key": os.getenv("G_PRIVATE_KEY"),
+    "client_email": os.getenv("G_CLIENT_EMAIL"),
     "client_id": os.getenv("G_CLIENT_ID"),
     "auth_uri": os.getenv("G_AUTH_URI"),
     "token_uri": os.getenv("G_TOKEN_URI"),
