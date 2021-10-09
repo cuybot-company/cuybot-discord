d0ae4d4f5ae0f6cf4f0434e682c357019cfc4d1c
dea <dea.aprizal@icloud.com>
2021-10-09 13:31:42 +0700

    securing token

--- helper/constants.py
+++ helper/constants.py
@@ -1,4 +1,4 @@
-import discord
+import discord, os
 from datetime import datetime
 
 request_help = ["cuy/help", "cuy/bantuan", "cuy/command"]
@@ -8,7 +8,17 @@ request_quote = ["cuy/quotes", "cuy/quote", "cuy/kutipan"]
 request_lyric = ["cuy/lirik", "cuy/lyric", "cuy/lyrics"]
 request_mobile = ["cuy/mobile", "cuy/hp", "cuy/handphone", "cuy/phone"]
 request_new_mobile = ["baru", "terbaru", "new"]
-
+request_g_credentials = {
+    "type": os.getenv("G_TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("G_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("G_PRIVATE_KEY"),
+    "client_id": os.getenv("G_CLIENT_ID"),
+    "auth_uri": os.getenv("G_AUTH_URI"),
+    "token_uri": os.getenv("G_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("G_AUTH_PROVIDER"),
+    "client_x509_cert_url": os.getenv("G_CLIENT_CERT")
+}
 data_covid_from = 'https://data.covid19.go.id'
 today = datetime.today().strftime('%YY-%MM-%DD')
 client = discord.Client()
