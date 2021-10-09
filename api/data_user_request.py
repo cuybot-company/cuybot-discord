89c6f9df467aecc929dd536da6ef1ffc7993c08c
dea <dea.aprizal@icloud.com>
2021-10-09 14:06:33 +0700

    securing token

--- api/data_user_request.py
+++ api/data_user_request.py
@@ -1,12 +1,10 @@
-import os, gspread, datetime
-import helper.constants as c
+import gspread
+import datetime
 time = datetime.datetime.now()
 
-credential = c.request_g_credentials
-
-gcloud_connect = gspread.service_account_from_dict(credential)
-gsheet = gcloud_connect.open(os.getenv('GSHEET_PROJECT_NAME')).sheet1
+cloud = gspread.service_account(filename='./config/gkt.json')
+excel = cloud.open('cuybot-discord-request').sheet1
 
 def insert(sender, request):
-    save_data = gsheet.append_row([str(sender), str(request), str(time)])
-    return save_data
+    save = excel.append_row([str(sender), str(request), str(time)])
+    return save
