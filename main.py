5f998ebc4a096abbc06db4fbbe2a474321d15827
Cendy <cendyemail@gmail.com>
2021-10-11 13:42:02 +0700

    Removed discord token

--- main.py
+++ main.py
@@ -65,5 +65,4 @@ async def on_message(message):
     await _reputation.check()
 
 liveserver()
-#c.client.run(os.getenv('TOKEN'))
-c.client.run('ODk2MzA1NDc1OTEyNjE4MDA0.YWFLhg.0VhUOcYN8do1ddO1HMEqpXFYk_0')
+c.client.run(os.getenv('TOKEN'))
