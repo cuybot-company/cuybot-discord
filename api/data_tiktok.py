cfb42102a6aa3553583db7f435a0e9a225b3d9bf
Rizsyad AR <42699914+Rizsyad@users.noreply.github.com>
2021-10-12 19:13:18 +0700

    update, fix bug, and more beautiful view

--- api/data_tiktok.py
+++ api/data_tiktok.py
@@ -6,30 +6,9 @@ def info(username):
         r = requests.get(f'https://info-tiktok.rizsyad.repl.co/{username}')
         json_data = json.loads(r.text)
         
-        if json_data["status"] == "false":
-            return(f"Username {username} tidak ditemukan")
+        if json_data["status"] == "error":
+            return json_data["message"]
 
         profile_data = json_data["data"]
 
-        bio = profile_data["bio"]
-        following = profile_data["following"]
-        followers = profile_data["followers"]
-        fans = profile_data["fans"]
-        hearts = profile_data["hearts"]
-        videos = profile_data["videos"]
-        verified = "Akun Sudah Verified" if profile_data["verified"] == True else "Akun Belum Verified"
-
-        message = """
-        :information_source: Informasi Akun TikTok :information_source: 
-        
-Link Tiktok: https://www.tiktok.com/{}
-bio: {}
-Following: {}
-Followers: {}
-Fans: {}
-Likes: {}
-Videos: {}
-Verified: {}
-    """.format(username, bio, following, followers, fans, hearts, videos, verified)
-
-        return message
+        return profile_data
