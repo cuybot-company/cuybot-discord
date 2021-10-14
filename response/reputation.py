b63fd8db2787e8d96e0d38b1e25d08ff1f9a8d7a
dea <dea.aprizal@icloud.com>
2021-10-14 12:16:42 +0700

    modif text reputation & remove unused help

--- response/reputation.py
+++ response/reputation.py
@@ -16,17 +16,17 @@ class Reputation:
             mention_self = str(self.sender.id) in args[0]
             if args[0] == 'help':
                 embed=discord.Embed(title="Reputation", description="List of argument needed for Reputation", color=0x50d396)
-                embed.add_field(name="Check user reputation", value="@mention", inline=True)
-                embed.add_field(name="Add/decrease user reputation", value="@mention *point*", inline=True)
+                embed.add_field(name="Untuk melihat reputasi member saat ini", value="@mention", inline=True)
+                embed.add_field(name="Untuk memberikan member point", value="@mention *point*", inline=True)
                 await self.bot_say(embed=embed)
             elif bool(id_pattern.match(args[0])) and len(args) < 2:
                 args[0] = args[0].replace('!', '')
-                await self.bot_say(f'{args[0]} ada {api.reputation_value(args[0])} point')
+                await self.bot_say(f':tada: reputasi {args[0]} saat ini: :tada:\n`{api.reputation_value(args[0])}** point`\n\n *tips: lempar reputasi ke member yang dirasa membantu komunitas ini menjadi lebih berkembang lagi.*\ngunakan `cuy/rep help` untuk penggunaan command reputasi.')
             elif isinstance(int(args[1]), int):
                 if mention_self:
-                    await self.bot_say('Not allowed')
+                    await self.bot_say('Sorry cuy gak bisa ngasih point ke diri sendiri ya!')
                 elif int(args[1]) > 10 or int(args[1]) < -10:
-                    await self.bot_say('Point not allowed')
+                    await self.bot_say('Maksimal cuma bisa lempar 10 point plus ataupun 10 point minus')
                 else :
                     args[0] = args[0].replace('!', '')
                     if args[1].startswith('+'):
@@ -35,6 +35,6 @@ class Reputation:
                     if cell == None:
                         api.reputation_insert(args[0])
                     api.reputation_update(args[0], int(args[1]))
-                    await self.bot_say(f'{args[0]} sekarang ada {api.reputation_value(args[0])} point')
+                    await self.bot_say(f':clap: thanks udah apresiasi {args[0]} dan menambahkan {str(args[1])} point :clap:')
         except:
             pass
