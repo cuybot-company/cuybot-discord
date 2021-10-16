a1ca86d59cac764ac75f85ea0d7e8e0b7fe4fd4f
Cendy <cendyemail@gmail.com>
2021-10-16 09:58:17 +0700

    move some import

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from config.liveserver import liveserver
 import os
 import locale
 import helper.constants as c
 
 locale.setlocale(locale.LC_ALL, '')
-from config.liveserver import liveserver
 liveserver()
-
 
 plugins = next(os.walk("./response"), (None, None, []))[2] 
 
