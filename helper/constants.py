e8f977197ab1fcc096a6cdc1f3f781676ae255f9
Tedi Suryadi <32906420+Teday@users.noreply.github.com>
2021-10-18 07:52:24 +0000

    generate avatar

--- helper/constants.py
+++ helper/constants.py
@@ -17,6 +17,7 @@ request_wallpaper = ["cuy/wp", "cuy/wallpaper"]
 request_word = ["cuy/dictionary", "cuy/kamus", "cuy/Kamus", "cuy/Dictionary", "cuy/dict"]
 request_face = ["cuy/tebak muka", "cuy/tebak wajah"]
 request_coffee = ["cuy/coffee", "cuy/coffee hari ini", "cuy/ngopi", "cuy/ngopi dulu"]
+request_avatar = ["cuy/avatar"]
 
 data_covid_from = 'https://data.covid19.go.id'
 today = datetime.today().strftime('%YY-%MM-%DD')
--- response/face.py
+++ response/face.py
@@ -17,6 +17,17 @@ class Face(c.cog):
       embed = discord.Embed(color = discord.Colour.red(),description= message[2] + ' wajahnya mirip ini kan?')
       embed.set_image(url = data[0]['url'])
       await bot_send(embed=embed)
+  @c.cmd.command(name="avatar")
+  async def avatar(self, ctx):
+    user_message = ctx.message.content
+    bot_send = ctx.message.reply
+    message = user_message.split(" ", 1)
+    if len(message) == 1:
+      await bot_send('masukan namanya dulu cuy')
+    else:
+      embed = discord.Embed(color = discord.Colour.red(),description= ' Avatar yang cocok buat ' + message[1])
+      embed.set_image(url = 'https://robohash.org/'+ message[1] + '?set=set2')
+      await bot_send(embed=embed)
 
 def setup(client):
     client.add_cog(Face(client))
