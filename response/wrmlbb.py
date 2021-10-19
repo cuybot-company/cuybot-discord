de0b9f3d56512ada1bc7ae734b345958825ca3a5
xcl.namee <mr3328496@gmail.com>
2021-10-19 16:35:46 +0700

    Update wrmlbb.py

--- response/wrmlbb.py
+++ response/wrmlbb.py
@@ -8,34 +8,34 @@ class wrmlbb(c.cog):
     async def itung(client, ctx):
         user_message = ctx.message.content
         bot_send = ctx.message.reply
-        data = user_message.split('', 3)
+        data = user_message.split(' ', 3)
 
-        tWin = data[1] * (data[2]/100)
-        tLose = data[1] - tWin
+        tWin = int(data[1]) * (int(data[2])/100)
+        tLose = int(data[1]) - tWin
         
         if len(data) == 1:
             await bot_send(f"Total match anda adalah {data[1]}")
         elif len(data) == 2:
           await bot_send(f'Total kemenangan : {tWin}\n Total kekalahan : {tLose}')        
         elif len(data) == 3:
-            if data[3] == 100:
+            if int(data[3]) == 100:
                 await bot_send('Ga bisa bro jgn maksa...')
-            elif data[3] >= 100:
-                await bot_send('Punten bang wr max 100%, kalo diatas 100%//itu diskon bang...')
-            elif (data[3] <= data[2]):
-                sisaWr = 100 - data[3]
+            elif int(data[3]) >= 100:
+                await bot_send('Punten bang wr max 100% "-_ ...')
+            elif (int(data[3]) <= int(data[2])):
+                sisaWr = 100 - int(data[3])
                 wrResult = 100 / int(sisaWr)
-                seratusPersen = tLose * int(wrResult)
-                final = int(seratusPersen) - data[3]
+                seratusPersen = int(tLose) * int(wrResult)
+                final = int(seratusPersen) - int(data[3])
                 lose = abs(final)
-                await bot_send(f'Anda membutuhkan {lose} lose untuk mendapatkan Win Rate {data[3]}%')
+                await bot_send(f'Anda membutuhkan {lose} lose untuk menjadikan Win Rate {data[3]}%')
             else:
-                sisaWr = 100 - data[3]
+                sisaWr = 100 - int(data[3])
                 wrResult = 100 / int(sisaWr)
-                seratusPersen = tLose * int(wrResult)
-                final = int(seratusPersen) - data[3]
+                seratusPersen = int(tLose) * int(wrResult)
+                final = int(seratusPersen) - int(data[3])
                 win = final
-                await bot_send(f'Anda membutuhkan {win} lose untuk mendapatkan Win Rate {data[3]}%')
+                await bot_send(f'Anda membutuhkan {win} win untuk menjadikan Win Rate {data[3]}%')
 
 def setup(client):
     client.add_cog(wrmlbb(client))
