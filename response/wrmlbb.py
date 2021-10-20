4f9986fc14e1f4483ad62d99b2f8fe8ab5d46f63
xcl.namee <mr3328496@gmail.com>
2021-10-20 11:02:33 +0700

    FIX BUG
    akhirnya bisa jg

--- response/wrmlbb.py
+++ response/wrmlbb.py
@@ -9,33 +9,42 @@ class wrmlbb(c.cog):
         user_message = ctx.message.content
         bot_send = ctx.message.reply
         data = user_message.split(' ', 3)
-
-        tWin = int(data[1]) * (int(data[2])/100)
-        tLose = int(data[1]) - tWin
-        
-        if len(data) == 1:
-            await bot_send(f"Total match anda adalah {data[1]}")
-        elif len(data) == 2:
-          await bot_send(f'Total kemenangan : {tWin}\n Total kekalahan : {tLose}')        
-        elif len(data) == 3:
-            if int(data[3]) == 100:
-                await bot_send('Ga bisa bro jgn maksa...')
-            elif int(data[3]) >= 100:
-                await bot_send('Punten bang wr max 100% "-_ ...')
-            elif (int(data[3]) <= int(data[2])):
-                sisaWr = 100 - int(data[3])
-                wrResult = 100 / int(sisaWr)
-                seratusPersen = int(tLose) * int(wrResult)
-                final = int(seratusPersen) - int(data[3])
-                lose = abs(final)
-                await bot_send(f'Anda membutuhkan {lose} lose untuk menjadikan Win Rate {data[3]}%')
+        # fungsi
+        if len(data) == 3:
+            match = int(data[1])
+            wrawal = int(data[2])
+            tWin = (match) * (wrawal/100)
+            tLose = (match) - (tWin)
+            await bot_send(f"Total kemenangan anda : {tWin} \nTotal kekalahan anda  : {tLose}")
+        elif len(data) == 4:
+            if data[3] < data[2]:
+                match = int(data[1])
+                wrawal = int(data[2])
+                wrakhir = int(data[3])
+                # var kalkulasi 
+                tWin = (match) * (wrawal/100)
+                tLose = (match) - (tWin)
+                sisaWr = 100 - (wrakhir)
+                wrResult = 100 / sisaWr
+                seratusPersen = (tLose) * (wrResult)
+                final = seratusPersen - match
+                lose = abs(int(final))
+                # await bot_send(f'{data[0]} ,{data[1]},{data[2]},{data[3]}')
+                await bot_send(f'Anda membutuhkan {lose} lose tanpa win untuk mendapatkan Win Rate {data[3]}%')
             else:
-                sisaWr = 100 - int(data[3])
-                wrResult = 100 / int(sisaWr)
-                seratusPersen = int(tLose) * int(wrResult)
-                final = int(seratusPersen) - int(data[3])
-                win = final
-                await bot_send(f'Anda membutuhkan {win} win untuk menjadikan Win Rate {data[3]}%')
-
+                match = int(data[1])
+                wrawal = int(data[2])
+                wrakhir = int(data[3])
+                tWin = (match) * (wrawal/100)
+                tLose = (match) - (tWin)
+                sisaWr = 100 - (wrakhir)
+                wrResult = 100 / sisaWr
+                seratusPersen = (tLose) * (wrResult)
+                final = seratusPersen - match
+                win = int(final)
+                await bot_send(f'Anda membutuhkan {win} win tanpa lose untuk mendapatkan Win Rate {data[3]}%')
+        else:
+            await bot_send("Command Help : cuy/wrcal [total match] [total wr] [target wr]")
+            
 def setup(client):
     client.add_cog(wrmlbb(client))
