4244681c2448166470d5970af3bc45db3e5b7804
Tedi Suryadi <32906420+Teday@users.noreply.github.com>
2021-10-21 04:02:39 +0000

    gacha cuy

--- response/gacha.py
+++ response/gacha.py
@@ -2,8 +2,10 @@ import api.data_user_request as api
 import random
 from random import choice
 import helper.constants as c
+import discord
 
 win = False
+your_try = []
 
 class Reward(c.cog):
   def __init__(self, client):
@@ -13,31 +15,36 @@ class Reward(c.cog):
     sender_id = ctx.message.author.id
     bot_send = ctx.message.reply
     num = random.randint(1, 100)
-    exist = api.reward_find(sender_id)
-    if exist == None:
-        api.reward_insert(sender_id, num)
-        await bot_send(f'<@{sender_id}> lu harus dapatin angka **{num}**, goodluck! ketik `cuy/gacha` untuk menebak angka')
+    if discord.utils.get(ctx.message.author.roles, name="Moderator 🔑"):    
+        await bot_send(f'<@{sender_id}> lu harus dapatin angka *{num}*, goodluck! ketik `cuy/gacha` untuk menebak angka')
+    else:
+        await bot_send('maaf lu gak punya akses untuk eksekusi command ini')
   
   @c.cmd.command(name="gacha")
   async def gacha(self, ctx):
-    your_try = []
+    global your_try
     sender_id = ctx.message.author.id
     bot_send = ctx.message.reply
     user_db_num = api.reward_value(sender_id)
     num = random.randint(1, 100)
-    if num not in your_try:
-        your_try.append(num)
-    selection = choice([num for num in range(1,100) if num not in your_try])
-    
-    if win or user_db_num == None:
-        return
-    print(str(selection))
+    i = 0
+    while i < 1:
+      if any(x in f'{num}' for x in your_try):
+        num = random.randint(1, 100)
+      else:
+        i = 1
+      if i == 1:
+        break
+    if len(your_try) == 0:
+      your_try = [f'{num}']
+    else:
+      your_try.append(f'{num}')
     
     if num == int(user_db_num):
         api.reward_win_time(sender_id)
         await bot_send(f'<@{sender_id}> WIN!!! :clap:')
     else :
-        await bot_send(f'{num}')
+        await bot_send(f'{num} Maaf kamu belum beruntung \nSilahkan coba lagi')
 
 def setup(client):
     client.add_cog(Reward(client))
