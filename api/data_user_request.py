652e0f9bf34766e47bf21dc1d3948bc14dfbaa85
dea <dea.aprizal@icloud.com>
2021-10-21 10:08:40 +0700

    test

--- api/data_user_request.py
+++ api/data_user_request.py
@@ -3,12 +3,12 @@ import datetime
 time = datetime.datetime.now()
 
 # name_exel = 'cuybot-discord-request'
-name_exel = 'discord-test'
+name_exel = 'cuybot-discord-request'
 cloud = gspread.service_account(filename='./config/gkt.json')
 
 excel = cloud.open(name_exel).sheet1
 reputation = cloud.open(name_exel).get_worksheet(1) # gspread only implement sheet1 so do it manually
-gacha = cloud.open(name_exel).get_worksheet(3)
+reward = cloud.open(name_exel).get_worksheet(2)
 
 def insert(sender, request):
     save = excel.append_row([str(sender), str(request), str(time)])
@@ -35,15 +35,18 @@ def reputation_update(id, point):
     reputation.update_cell(cell.row, 2, str(int(value) + point))
 
 def reward_insert(id, num):
-    reward.append_row([str(id), num])
+    reward.append_row([str(id), num, '-'])
+    
 def reward_find(id):
     cell = reward.find(str(id))
     return cell
+
 def reward_value(id):
     cell = reward_find(id)
     if cell != None:
         val = reward.cell(cell.row, 2)
         return val.value
+    
 def reward_check(id):
     cell = reward.find(id)
     if cell != None:
@@ -51,28 +54,32 @@ def reward_check(id):
         return val.value
     else:
         return '0'
-
-def gacha_insert(id, number):
-    gacha.append_row([str(id), number])
-
-def gacha_find(id):
-    cell = gacha.find(id)
-    return cell
-
-def gacha_tebak(id, number):
-    num_rows = gacha.row_count - 1
-    curr_row = 0
-    arr = []
-
-    while curr_row < num_rows:
-        try:
-            curr_row += 1
-            row = gacha.row_values(curr_row)
-            arr.append(row)
-        except IndexError:
-            break
     
-    id_check = any(id in id_list for id_list in arr)
-    number_check = any(number in number_list for number_list in arr)
+def reward_win_time(id):
+    cell = reward_find(id)
+    reward.update_cell(cell.row, 3, str(time))
+    
+# def gacha_insert(id, number):
+#     gacha.append_row([str(id), number])
 
-    return (id_check and number_check)
+# def gacha_find(id):
+#     cell = gacha.find(id)
+#     return cell
+
+# def gacha_tebak(id, number):
+#     num_rows = gacha.row_count - 1
+#     curr_row = 0
+#     arr = []
+
+#     while curr_row < num_rows:
+#         try:
+#             curr_row += 1
+#             row = gacha.row_values(curr_row)
+#             arr.append(row)
+#         except IndexError:
+#             break
+    
+#     id_check = any(id in id_list for id_list in arr)
+#     number_check = any(number in number_list for number_list in arr)
+
+#     return (id_check and number_check)
